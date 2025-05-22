8b60be70fec057bf03010c0cfc154bdd81df694f
Bruno Franco <brunofrancco@gmail.com>
2025-05-22 23:00:24 +0100

    ajustes para vercel, inclusao vercelignore e melhorias no server

--- api/index.js
+++
@@ -1,3 +0,0 @@
-const app = require('../dist/src/app').default;
-
-module.exports = app;
--- dist/api/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const app = require('../dist/src/app').default;
-module.exports = app;
-//# sourceMappingURL=index.js.map
