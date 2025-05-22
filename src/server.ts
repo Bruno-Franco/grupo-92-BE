dc9c495289f4ef8e293cec35e43e93c25cb352c5
Bruno Franco <brunofrancco@gmail.com>
2025-05-22 23:54:12 +0100

    ajustes para vercel, inclusao vercelignore e melhorias no server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import app from './app';
 import config from './config/config';
 
-if (config.nodeEnv !== 'production') {
-  const PORT = config.port;
-  app.listen(PORT, () => {
-    console.log(`Server listenning on port ${PORT}`);
-  });
-}
-export default app;
+const PORT = config.port;
+app.listen(PORT, () => {
+  console.log(`Server listenning on port ${PORT}`);
+});
